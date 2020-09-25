6870aaa0bae0f80cad3485ac20bde5ddc2574e76
Thomas A <curioustommy@protonmail.com>
2020-09-24 21:18:50 -0700

    Generic Arch Build Idea

--- cmake/darling_static_lib.cmake
+++ cmake/darling_static_lib.cmake
@@ -4,7 +4,7 @@ cmake_policy(SET CMP0063 NEW)
 include(darling_lib)
 
 function(add_darling_static_library name)
-	cmake_parse_arguments(STATIC_LIB "FAT;x86_64_ONLY;i386_ONLY" "" "SOURCES" ${ARGN})
+	cmake_parse_arguments(STATIC_LIB "FAT;64BIT_ONLY;32BIT_ONLY" "" "SOURCES" ${ARGN})
 
 	set(CMAKE_AR "${CMAKE_BINARY_DIR}/src/external/cctools-port/cctools/ar/x86_64-apple-darwin11-ar")
 	set(CMAKE_RANLIB "${CMAKE_BINARY_DIR}/src/external/cctools-port/cctools/ar/x86_64-apple-darwin11-ranlib")
@@ -14,14 +14,14 @@ function(add_darling_static_library name)
 	set_property(TARGET ${name} APPEND_STRING PROPERTY LINK_FLAGS " -B ${CMAKE_BINARY_DIR}/src/external/cctools-port/cctools/misc/")
 	add_dependencies(${name} lipo)
 
-	if (TARGET_x86_64 AND NOT STATIC_LIB_i386_ONLY)
+	if (BUILD_TARGET_64BIT AND NOT STATIC_LIB_32BIT_ONLY)
 		set_property(TARGET ${name} APPEND_STRING PROPERTY
-			COMPILE_FLAGS " -arch x86_64")
+			COMPILE_FLAGS " -arch ${APPLE_ARCH_64BIT}")
 	endif ()
 
-	if (TARGET_i386 AND NOT STATIC_LIB_x86_64_ONLY)
+	if (BUILD_TARGET_32BIT AND NOT STATIC_LIB_64BIT_ONLY)
 		set_property(TARGET ${name} APPEND_STRING PROPERTY
-			COMPILE_FLAGS " -arch i386")
+			COMPILE_FLAGS " -arch ${APPLE_ARCH_32BIT}")
 	endif ()
 
 	if (STATIC_LIB_FAT)
